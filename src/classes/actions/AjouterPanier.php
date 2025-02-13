d233272759de7d5185706e9ac6938877cea6531d
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2025-02-13 20:05:05 +0100

    ajout possibilité de sup/ajout

--- src/classes/actions/AjouterPanier.php
+++ src/classes/actions/AjouterPanier.php
@@ -10,6 +10,7 @@ use iutnc\PiloteAndCo\models\Panier as Panier;
 class AjouterPanier extends Action{
     public function execute(): string{
         if(isset($_SESSION["user"]) && isset($_GET["id"])){
+            $quantite = isset($_GET["quantite"]) && $_GET["quantite"] !== '' ? (int)$_GET["quantite"] : 0;
             $user = unserialize($_SESSION["user"]);
             $id_produit = (int) $_GET["id"];
             Panier::ajouterPanier((int)$user->id, (int)$id_produit, 1);
