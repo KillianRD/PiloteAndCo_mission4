a36ca1903198fe68e45925fa0e868eeef1426e6e
Thomas Charpentier <thomas57.charpentier@gmail.com>
2025-02-13 14:12:03 +0100

    Début header

--- src/classes/dispatch/Dispatcher.php
+++ src/classes/dispatch/Dispatcher.php
@@ -23,13 +23,36 @@ class Dispatcher
         $this->renderPage($html);
     }
 
-    private function renderPage(string $html) : void
+    private function renderPage(string $html): void
     {
         echo <<<END
-            
-            Salut, je suis la page de ecommerce de Pilote&Co
-            
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Réstore</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        </head>
+            <body>
+                <header>
+                    <div class="top-bar">
+                        <div class="logo">
+                            <a href="index.php"><img src="test.png" alt="logo"/></a>
+                        </div>
+                        <div class="auth">
+                            <a href="index.php&action=login">Login</a>
+                        </div>
+                    </div>
+                    <nav class="nav-bar">
+                        <a href="index.php">Accueil</a>
+                    </nav>
+                </header>
+        END
+            . $html .
+            <<<END
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-gH0i7tq3bJwSD6GfXn985OJXmd0Y5I8au3/UoxZCGLFqGF4twkwRjSMP9QSDRNNx" crossorigin="anonymous"></script>
+            </body>
+        </html>
         END;
-
     }
 }
