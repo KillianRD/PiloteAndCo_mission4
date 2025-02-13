fc59931e179f4f8dc495851511e58106a9150301
Thomas Charpentier <thomas57.charpentier@gmail.com>
2025-02-13 17:40:37 +0100

    Avancement

--- src/classes/dispatch/Dispatcher.php
+++ src/classes/dispatch/Dispatcher.php
@@ -1,25 +1,44 @@
 <?php
 
-    namespace iutnc\PiloteAndCo\dispatch;
+namespace iutnc\PiloteAndCo\dispatch;
 
     use iutnc\PiloteAndCo\actions\Accueil;
     use iutnc\PiloteAndCo\actions\LoginAction;
     use iutnc\PiloteAndCo\actions\Logout;
+    use iutnc\PiloteAndCo\actions\ParcourirCategorie;
     use iutnc\PiloteAndCo\actions\RegisterAction;
 
-    class Dispatcher
-{
-        private ?string $action;
 
-        public function __construct()
-        {
-            $this->action = $_GET['action'] ?? null;
-        }
+class Dispatcher
+{
+    private ?string $action;
+
+    public function __construct()
+    {
+        $this->action = $_GET['action'] ?? null;
+    }
+
     public function run(): void
     {
         $html = "";
 
         switch ($this->action) {
+            case "electromenager":
+                $a = new ParcourirCategorie("electromenager");
+                $html .= $a->execute();
+                break;
+            case "jardinage":
+                $a = new ParcourirCategorie("jardinage");
+                $html .= $a->execute();
+                break;
+            case "literie":
+                $a = new ParcourirCategorie("literie");
+                $html .= $a->execute();
+                break;
+            case "mobilier":
+                $a = new ParcourirCategorie("mobilier");
+                $html .= $a->execute();
+                break;
             case "home" :
             case "login" :
                 $a = new LoginAction();
@@ -33,10 +52,10 @@
             default :
                 $a = new Accueil();
                 break;
-            }
-            $html .= $a->execute();
-            $this->renderPage($html);
         }
+        $html .= $a->execute();
+        $this->renderPage($html);
+    }
 
         private function afficherLoginOrProfil():string{
             if(isset($_SESSION['user'])){
@@ -78,11 +97,13 @@
             <head>
                 <meta charset="UTF-8">
                 <title>Réstore</title>
-                <link href="./css/bootstrap_css/bootstrap.css" rel="stylesheet" crossorigin="anonymous">                <link rel="preconnect" href="https://fonts.googleapis.com">
+
+                <link href="./css/bootstrap_css/bootstrap.css" rel="stylesheet" crossorigin="anonymous">
+                <link rel="preconnect" href="https://fonts.googleapis.com">
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
                 <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
-                <link rel="stylesheet" href="../../../css/index.css">
+                <link rel="stylesheet" href="./css/index.css">
             </head>
             <body>
                 <header style="height: 120px;">
@@ -90,7 +111,7 @@
                         <!-- Logo à gauche -->
                         <div>
                             <a href="index.php">
-                                <img src="/images/logo.png" alt="logo" style="height: 20em;">
+                                <img src="./images/logo.png" alt="logo" style="height: 20em;">
                             </a>
                         </div>
                         <!-- Thèmes des produits au centre -->
@@ -109,6 +130,8 @@
                 </header>
             $html
                 <script src="./js/bootstrap.bundle.js" crossorigin="anonymous"></script>
+
+
             </body>
         </html>
         END;
