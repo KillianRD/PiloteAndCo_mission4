d0cedbefd2d271acd70593aaf6af4317c599eaa2
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2025-02-13 15:46:52 +0100

    ajout de la classe panier

---
+++ src/classes/models/Panier.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace iutnc\PiloteAndCo\models;
+
+class Panier{
+    private int $id_utilisateur;
+    private int $id_produit;
+    private int $quantite;
+
+    public function __construct($id_u, $id_p, $q){
+        $this->id_utilisateur = $id_u;
+        $this->id_produit = $id_p;
+        $this->quantite = $q;
+    }
+
+    public function __get(string $at): mixed
+    {
+        if (property_exists($this, $at)) {
+            return $this->$at;
+        }
+
+        throw new InvalidPropertyNameException("$at: propriété inconnue");
+    }
+    public function __set(string $at, mixed $val = null)
+    {
+        if (property_exists($this, $at)) {
+            $this->$at = $val;
+        } else {
+            throw new InvalidPropertyNameException(get_called_class() . " attribut invalid" . $at);
+        }
+    }
+}
