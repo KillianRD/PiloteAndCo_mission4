f6f47389a4d5e30b1c054d037def6340f58229c6
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2025-02-13 20:09:17 +0100

    modif panier

--- src/classes/actions/AjouterPanier.php
+++ src/classes/actions/AjouterPanier.php
@@ -3,7 +3,7 @@
 namespace iutnc\PiloteAndCo\actions;
 
 use iutnc\PiloteAndCo\actions\Action as Action;
-use iutnc\PiloteAndCo\actions\Accueil as Accueil;
+use iutnc\PiloteAndCo\actions\ParcourirPanier;
 use iutnc\PiloteAndCo\models\Panier as Panier;
 
 
@@ -14,8 +14,8 @@ class AjouterPanier extends Action{
             $user = unserialize($_SESSION["user"]);
             $id_produit = (int) $_GET["id"];
             Panier::ajouterPanier((int)$user->id, (int)$id_produit, 1);
-            $accueil = new Accueil();
-            return $accueil->execute();
+            $panier = new ParcourirPanier();
+            return $panier->execute();
         }
     }
 }
